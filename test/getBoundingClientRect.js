7e77cca3a9d592fa1b531986582b26107866df81
Kei Ito <kei.itof@gmail.com>
2016-09-23 14:23:29 +0900

    add getBoundingClientRect

---
+++ test/getBoundingClientRect.js
@@ -0,0 +1,10 @@
+describe('getBoundingClientRect', function () {
+
+	var assert = require('assert');
+	var getBoundingClientRect = require('../lib/getBoundingClientRect');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
