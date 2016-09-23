d7e86c09f610f486c27822c6e3d71eea9effa307
Kei Ito <kei.itof@gmail.com>
2016-09-23 16:06:52 +0900

    add scrollX/Y

---
+++ test/scrollX.js
@@ -0,0 +1,10 @@
+describe('scrollX', function () {
+
+	var assert = require('assert');
+	var scrollX = require('../lib/scrollX');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
---
+++ test/scrollY.js
@@ -0,0 +1,10 @@
+describe('scrollY', function () {
+
+	var assert = require('assert');
+	var scrollY = require('../lib/scrollY');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
