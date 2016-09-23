7e6e6909a08771f4772962328c685dcda25176a0
Kei Ito <kei.itof@gmail.com>
2016-09-23 14:20:26 +0900

    add setStyle

---
+++ test/setStyle.js
@@ -0,0 +1,10 @@
+describe('setStyle', function () {
+
+	var assert = require('assert');
+	var setStyle = require('../lib/setStyle');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
