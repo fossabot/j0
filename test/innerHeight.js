2241e0e6645997d8eea58c3e5162d7faff56882f
Kei Ito <kei.itof@gmail.com>
2016-09-23 14:22:16 +0900

    add innerHeight/Width

---
+++ test/innerHeight.js
@@ -0,0 +1,10 @@
+describe('innerHeight', function () {
+
+	var assert = require('assert');
+	var innerHeight = require('../lib/innerHeight');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
---
+++ test/innerWidth.js
@@ -0,0 +1,10 @@
+describe('innerWidth', function () {
+
+	var assert = require('assert');
+	var innerWidth = require('../lib/innerWidth');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
