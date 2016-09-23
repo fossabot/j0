4ee7dc063df86f14ef4b5d7e50d675fabe6ccff6
Kei Ito <kei.itof@gmail.com>
2016-09-23 20:29:17 +0900

    add debounce and getElementsByTagName

---
+++ test/debounce.js
@@ -0,0 +1,10 @@
+describe('debounce', function () {
+
+	var assert = require('assert');
+	var debounce = require('../lib/debounce');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
---
+++ test/getElementsByTagName.js
@@ -0,0 +1,10 @@
+describe('getElementsByTagName', function () {
+
+	var assert = require('assert');
+	var getElementsByTagName = require('../lib/getElementsByTagName');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
