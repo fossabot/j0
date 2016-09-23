c85428d826e07253d0675180d628ba2d077c6d14
Kei Ito <kei.itof@gmail.com>
2016-09-23 14:27:42 +0900

    add children

---
+++ test/children.js
@@ -0,0 +1,10 @@
+describe('children', function () {
+
+	var assert = require('assert');
+	var children = require('../lib/children');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
