e46fa55ce5a0efd5c5c0786ea0f73dcfc847c3d3
Kei Ito <kei.itof@gmail.com>
2016-09-26 19:50:36 +0900

    add concat

---
+++ test/concat.js
@@ -0,0 +1,10 @@
+describe('concat', function () {
+
+	var assert = require('assert');
+	var concat = require('../lib/concat');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
