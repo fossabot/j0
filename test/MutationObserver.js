ca4eb721af2978987721435f944864be6b691c64
Kei Ito <kei.itof@gmail.com>
2016-09-27 21:26:51 +0900

    add MutationObserver

---
+++ test/MutationObserver.js
@@ -0,0 +1,10 @@
+describe('MutationObserver', function () {
+
+	var assert = require('assert');
+	var MutationObserver = require('../lib/MutationObserver');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
---
+++ test/removeEventListener.js
@@ -0,0 +1,10 @@
+describe('removeEventListener', function () {
+
+	var assert = require('assert');
+	var removeEventListener = require('../lib/removeEventListener');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
