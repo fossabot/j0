115b975c9860200bc0e2c654626a85b0bf9d07fc
Kei Ito <kei.itof@gmail.com>
2016-09-28 12:41:36 +0900

    add first/last Child

---
+++ test/firstChild.js
@@ -0,0 +1,10 @@
+describe('firstChild', function () {
+
+	var assert = require('assert');
+	var firstChild = require('../lib/firstChild');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
---
+++ test/lastChild.js
@@ -0,0 +1,10 @@
+describe('lastChild', function () {
+
+	var assert = require('assert');
+	var lastChild = require('../lib/lastChild');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
