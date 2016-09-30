e5c653f998ca3dbfd35f6a59741d56e8a944dd45
Kei Ito <kei.itof@gmail.com>
2016-09-30 20:35:59 +0900

    fix xxxClass and add replaceChild

---
+++ test/replaceChild.js
@@ -0,0 +1,10 @@
+describe('replaceChild', function () {
+
+	var assert = require('assert');
+	var replaceChild = require('../lib/replaceChild');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
