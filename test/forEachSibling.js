d659d61f73e76ed172dd7d70268cd658567684c2
Kei Ito <kei.itof@gmail.com>
2016-10-04 17:13:42 +0900

    add forEachSibling

---
+++ test/forEachSibling.js
@@ -0,0 +1,10 @@
+describe('forEachSibling', function () {
+
+	var assert = require('assert');
+	var forEachSibling = require('../lib/forEachSibling');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
