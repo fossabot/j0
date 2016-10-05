df2751ecf7eada1b00770105e758cef90de0b241
Kei Ito <kei.itof@gmail.com>
2016-10-05 20:42:52 +0900

    add triggerSibling

---
+++ test/triggerNextSibling.js
@@ -0,0 +1,10 @@
+describe('triggerNextSibling', function () {
+
+	var assert = require('assert');
+	var triggerNextSibling = require('../lib/triggerNextSibling');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
---
+++ test/triggerPreviousSibling.js
@@ -0,0 +1,10 @@
+describe('triggerPreviousSibling', function () {
+
+	var assert = require('assert');
+	var triggerPreviousSibling = require('../lib/triggerPreviousSibling');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
