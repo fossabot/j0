56b83dcf64cd78da875e9ad74e2b73be35f11bb1
Kei Ito <kei.itof@gmail.com>
2016-10-07 22:04:23 +0900

    add getBackgroundImage

---
+++ test/getBackgroundImage.js
@@ -0,0 +1,10 @@
+describe('getBackgroundImage', function () {
+
+	var assert = require('assert');
+	var getBackgroundImage = require('../lib/getBackgroundImage');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
