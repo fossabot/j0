4a245048826dd138a378b42a10d7cb2a1b58c63a
Kei Ito <kei.itof@gmail.com>
2016-10-12 16:06:09 +0900

    add DocumentFragment

---
+++ test/DocumentFragment.js
@@ -0,0 +1,10 @@
+describe('DocumentFragment', function () {
+
+	var assert = require('assert');
+	var DocumentFragment = require('../lib/DocumentFragment');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
