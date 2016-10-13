c73b7e6c98393f3bbf142557a71e352d842dcbae
Kei Ito <kei.itof@gmail.com>
2016-10-13 20:04:35 +0900

    add isBlob

---
+++ test/isBlob.js
@@ -0,0 +1,10 @@
+describe('isBlob', function () {
+
+	var assert = require('assert');
+	var isBlob = require('../lib/isBlob');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
