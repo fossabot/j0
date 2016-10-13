c50b50de8e13a8d2769410c513f7cd6edc5af86d
Kei Ito <kei.itof@gmail.com>
2016-10-13 16:25:44 +0900

    add shuffle

---
+++ test/shuffle.js
@@ -0,0 +1,10 @@
+describe('shuffle', function () {
+
+	var assert = require('assert');
+	var shuffle = require('../lib/shuffle');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
