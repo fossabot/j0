fd0036145e1860916d30da44621d1181e336b49c
Kei Ito <kei.itof@gmail.com>
2016-10-14 16:10:13 +0900

    add tagName

---
+++ test/tagName.js
@@ -0,0 +1,10 @@
+describe('tagName', function () {
+
+	var assert = require('assert');
+	var tagName = require('../lib/tagName');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
