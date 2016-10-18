eb2ad302fb0a9514e4bc0c5be7af4b208e40bfc8
Kei Ito <kei.itof@gmail.com>
2016-10-18 17:44:17 +0900

    update applyOrientation

---
+++ test/encodeCharacterReference.js
@@ -0,0 +1,10 @@
+describe('encodeCharacterReference', function () {
+
+	var assert = require('assert');
+	var encodeCharacterReference = require('../lib/encodeCharacterReference');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
