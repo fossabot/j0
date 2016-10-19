9a36b6a9840cf89668b6dba0b8d82fda2b85f147
Kei Ito <kei.itof@gmail.com>
2016-10-19 16:22:02 +0900

    add recursiveForEach

---
+++ test/recursiveForEach.js
@@ -0,0 +1,10 @@
+describe('recursiveForEach', function () {
+
+	var assert = require('assert');
+	var recursiveForEach = require('../lib/recursiveForEach');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
