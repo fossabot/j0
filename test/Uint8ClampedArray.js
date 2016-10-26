63eabb037a835e49123ab17d3d7ecdf04724de4c
Kei Ito <kei.itof@gmail.com>
2016-10-26 18:57:16 +0900

    add callbackPromise and Uint8ClampedArray

---
+++ test/Uint8ClampedArray.js
@@ -0,0 +1,10 @@
+describe('Uint8ClampedArray', function () {
+
+	var assert = require('assert');
+	var Uint8ClampedArray = require('../lib/Uint8ClampedArray');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
---
+++ test/callbackPromise.js
@@ -0,0 +1,10 @@
+describe('callbackPromise', function () {
+
+	var assert = require('assert');
+	var callbackPromise = require('../lib/callbackPromise');
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
