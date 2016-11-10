c480f23ce921d0bc21a4552d2edf1b2fc3a52ef3
Kei Ito <kei.itof@gmail.com>
2016-11-10 14:16:51 +0900

    add test:getQueryString

--- test/getQueryString.js
+++ test/getQueryString.js
@@ -1,10 +1,13 @@
+var assert = require('assert');
+var getQueryString = require('../lib/getQueryString');
+
 describe('getQueryString', function () {
 
-	var assert = require('assert');
-	var getQueryString = require('../lib/getQueryString');
-
-	it('should do X', function () {
-		assert.equal(0, 1);
+	it('should return a query string', function () {
+		assert.equal(getQueryString([
+			['name', 'Gapplin'],
+			['名前', 'ガップリン']
+		]), 'name=Gapplin&%E5%90%8D%E5%89%8D=%E3%82%AC%E3%83%83%E3%83%97%E3%83%AA%E3%83%B3');
 	});
 
 });
