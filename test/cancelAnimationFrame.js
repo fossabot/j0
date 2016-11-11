dd60d2a1da157eb43da33281aba57a118f8b3133
Kei Ito <kei.itof@gmail.com>
2016-11-11 19:19:13 +0900

    add test:cancelAnimationFrame, childNodes

--- test/cancelAnimationFrame.js
+++ test/cancelAnimationFrame.js
@@ -1,10 +1,34 @@
+var requestAnimationFrame = require('../lib/requestAnimationFrame');
+var cancelAnimationFrame = require('../lib/cancelAnimationFrame');
+var setTimeout = require('../lib/setTimeout');
+var Error = require('../lib/Error');
+var noop = require('../lib/noop');
+
 describe('cancelAnimationFrame', function () {
 
-	var assert = require('assert');
-	var cancelAnimationFrame = require('../lib/cancelAnimationFrame');
+	it('should not cancel calling', function (done) {
+		var onEnd = function (error) {
+			done(error);
+			onEnd = noop;
+		};
+		requestAnimationFrame(function () {
+			onEnd();
+		});
+		setTimeout(function () {
+			onEnd(new Error('Called unexpectedly'));
+		}, 50);
+	});
 
-	it('should do X', function () {
-		assert.equal(0, 1);
+	it('should cancel calling', function (done) {
+		var onEnd = function (error) {
+			done(error);
+			onEnd = noop;
+		};
+		var requestId = requestAnimationFrame(function () {
+			onEnd(new Error('Called unexpectedly'));
+		});
+		cancelAnimationFrame(requestId);
+		setTimeout(onEnd, 50);
 	});
 
 });
--- test/childNodes.js
+++ test/childNodes.js
@@ -1,10 +1,21 @@
+var assert = require('assert');
+var childNodes = require('../lib/childNodes');
+var createElement = require('../lib/createElement');
+var map = require('../lib/map');
+
 describe('childNodes', function () {
 
-	var assert = require('assert');
-	var childNodes = require('../lib/childNodes');
-
-	it('should do X', function () {
-		assert.equal(0, 1);
+	it('should return child nodes', function () {
+		var nodes = map([
+			'Wyvern',
+			{
+				c: 'Whackamole'
+			},
+			'Urnite'
+		], createElement);
+		assert.deepEqual(childNodes(createElement({
+			c: nodes
+		})), nodes);
 	});
 
 });
