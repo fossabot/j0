56b7a398741832bff04fca398c8e6bfa6a3af47d
Kei Ito <kei.itof@gmail.com>
2016-11-11 18:15:52 +0900

    add test: callbackPromise

--- test/callbackPromise.js
+++ test/callbackPromise.js
@@ -1,10 +1,27 @@
+var assert = require('assert');
+var callbackPromise = require('../lib/callbackPromise');
+var Error = require('../lib/Error');
+
 describe('callbackPromise', function () {
 
-	var assert = require('assert');
-	var callbackPromise = require('../lib/callbackPromise');
+	it('should be then-able', function (done) {
+		callbackPromise(function (callback) {
+			callback(null, 'Ramada');
+		}).then(function (data) {
+			assert.equal(data, 'Ramada');
+			done();
+		}).catch(done);
+	});
 
-	it('should do X', function () {
-		assert.equal(0, 1);
+	it('should be catch-able', function (done) {
+		callbackPromise(function (callback) {
+			callback('Rogue Armor');
+		}).then(function () {
+			done(new Error('Called unexpectedly'));
+		}).catch(function (data) {
+			assert.equal(data, 'Rogue Armor');
+			done();
+		});
 	});
 
 });
