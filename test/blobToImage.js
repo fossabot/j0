958ff1cba4aee37fc62a5c9b111d507c941281e0
Kei Ito <kei.itof@gmail.com>
2016-11-11 18:10:30 +0900

    add test: blobToImage

--- test/blobToImage.js
+++ test/blobToImage.js
@@ -1,10 +1,20 @@
+var assert = require('assert');
+var blobToImage = require('../lib/blobToImage');
+var http = require('../lib/http');
+var naturalWidth = require('../lib/naturalWidth');
+var naturalHeight = require('../lib/naturalHeight');
+
 describe('blobToImage', function () {
 
-	var assert = require('assert');
-	var blobToImage = require('../lib/blobToImage');
-
-	it('should do X', function () {
-		assert.equal(0, 1);
+	it('should create an image element from a blob', function (done) {
+		http({
+			method: 'GET',
+			url: 'files/j0-1.jpg',
+			responseType: 'blob'
+		}).then(blobToImage).then(function (img) {
+			assert.equal(0 < naturalWidth(img) * naturalHeight(img), true);
+			done();
+		}).catch(done);
 	});
 
 });
