1bf50f6334f329d18d96bb1f17cf4bba0fe6bf19
Kei Ito <kei.itof@gmail.com>
2016-12-05 10:46:29 +0900

    add confirm and prompt

---
+++ test/confirm.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var confirm = require('../lib/confirm');
+
+describe('confirm', function () {
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
---
+++ test/prompt.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var prompt = require('../lib/prompt');
+
+describe('prompt', function () {
+
+	it('should do X', function () {
+		assert.equal(0, 1);
+	});
+
+});
