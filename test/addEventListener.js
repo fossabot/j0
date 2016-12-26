0f9f18d47d0f889c8a3b94afbbf2229d37fde1c1
Kei Ito <kei.itof@gmail.com>
2016-12-26 11:16:18 +0900

    use middleware

--- test/addEventListener.js
+++ test/addEventListener.js
@@ -5,6 +5,10 @@ var addEventListener = require('../lib/addEventListener');
 
 describe('addEventListener', function () {
 
+	beforeEach(function () {
+		delete addEventListener.middleware;
+	});
+
 	it('should add a new listener', function (done) {
 		var element = createElement({});
 		addEventListener(element, 'Archmage', function () {
@@ -17,6 +21,17 @@ describe('addEventListener', function () {
 		var element = createElement({});
 		var fn = function () {};
 		assert.deepEqual(addEventListener(element, 'Archmage', fn), [[element, 'Archmage', fn]]);
+	});
+
+	it('should supports middleware', function () {
+		var element = createElement({});
+		var fn = function () {};
+		var called;
+		addEventListener.middleware = function (object, eventName, fn) {
+			called = [object, eventName, fn];
+		};
+		addEventListener(element, 'Archmage', fn);
+		assert.deepEqual(called, [element, 'Archmage', fn]);
 	});
 
 });
