b827ef7e1992da1168fed8817815769c4d211023
Kei Ito <kei.itof@gmail.com>
2017-01-31 13:39:18 +0900

    add tests for decodeURI

--- test/decodeURI.js
+++ test/decodeURI.js
@@ -1,10 +1,10 @@
+var assert = require('assert');
+var decodeURI = require('../lib/decodeURI');
+
 describe('decodeURI', function () {
 
-	var assert = require('assert');
-	var decodeURI = require('../lib/decodeURI');
-
-	it('should do X', function () {
-		assert.equal(0, 1);
+	it('should decode encoded URI', function () {
+		assert.equal(decodeURI('https://example.123/%E3%83%94%E3%83%83%E3%82%AD%E3%83%BC'), 'https://example.123/ピッキー');
 	});
 
 });
--- test/decodeURIComponent.js
+++ test/decodeURIComponent.js
@@ -1,10 +1,10 @@
+var assert = require('assert');
+var decodeURIComponent = require('../lib/decodeURIComponent');
+
 describe('decodeURIComponent', function () {
 
-	var assert = require('assert');
-	var decodeURIComponent = require('../lib/decodeURIComponent');
-
-	it('should do X', function () {
-		assert.equal(0, 1);
+	it('should decode encoded URI', function () {
+		assert.equal(decodeURIComponent('%E3%83%94%E3%83%83%E3%82%AD%E3%83%BC'), 'ピッキー');
 	});
 
 });
