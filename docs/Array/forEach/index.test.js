da83e0e3ae09e430364e398ceb638df529307476
Kei Ito <kei.itof@gmail.com>
2017-03-24 13:00:14 +0900

    add tests for Array/forEach

--- docs/Array/forEach/index.test.js
+++ docs/Array/forEach/index.test.js
@@ -38,10 +38,68 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 		}
 	}
 
-	it('should iterate an array', function () {
+	function push(arrayLike) {
+		var _Array$prototype$push;
+
+		for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+			args[_key - 1] = arguments[_key];
+		}
+
+		return (_Array$prototype$push = Array.prototype.push).call.apply(_Array$prototype$push, [arrayLike].concat(args));
+	}
+
+	it('should iterate over an array', function () {
 		var array = [1, 2, 3];
 		forEach(array, function (value, index, arr) {
 			assert.deepEqual([value, arr], [array[index], arr]);
 		});
+	});
+
+	it('should iterate over an array-like', function () {
+		var array = {
+			0: 1,
+			1: 2,
+			2: 3,
+			length: 3
+		};
+		forEach(array, function (value, index, arr) {
+			assert.deepEqual([value, arr], [array[index], arr]);
+		});
+	});
+
+	it('should iterate over an iterable', function () {
+		var count = 0;
+		var iterable = {
+			next: function next() {
+				count += 1;
+				return {
+					value: count,
+					done: 4 < count
+				};
+			}
+		};
+		var results = [];
+		forEach(iterable, function (value, index, arr) {
+			push(results, [value, index, arr]);
+		});
+		assert.deepEqual(results, [[1, 0, iterable], [2, 1, iterable], [3, 2, iterable], [4, 3, iterable]]);
+	});
+
+	it('should iterate over a string', function () {
+		var text = 'abcd';
+		var results = [];
+		forEach(text, function (value, index, arr) {
+			push(results, [value, index, arr]);
+		});
+		assert.deepEqual(results, [['a', 0, text], ['b', 1, text], ['c', 2, text], ['d', 3, text]]);
+	});
+
+	it('should call fn in a specified context', function () {
+		function fn(value) {
+			this.sum += value;
+		}
+		var context = { sum: 0 };
+		forEach([0, 1, 2, 3, 4, 5], fn, context);
+		assert.deepEqual(context, { sum: 15 });
 	});
 });
