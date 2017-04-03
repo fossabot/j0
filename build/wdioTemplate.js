dea4d63f5ed93c7e0cbbea1968c7250485782c71
Kei Ito <kei.itof@gmail.com>
2017-04-03 11:21:04 +0900

    increases instances

--- build/wdioTemplate.js
+++ build/wdioTemplate.js
@@ -3,8 +3,8 @@ const chalk = require('chalk');
 const console = require('j1/console').create('wdio');
 
 const lengthToRemoveArrowMark = -2;
-const waitBody = 1000;
-const retry = 20;
+const waitBody = 3000;
+const retry = 10;
 
 browser.setViewportSize({
 	width: 600,
