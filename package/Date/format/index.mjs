9758738e0f2151e05ecd363246a3759afa45e349
Kei Ito <kei.itof@gmail.com>
2017-04-10 11:02:37 +0900

    fixes formatDate to return an empty string is the arguments are invalid

--- package/Date/format/index.mjs
+++ package/Date/format/index.mjs
@@ -31,27 +31,30 @@ const DayNames = [
 
 function format(src, template) {
 	const date = new Date(src);
-	const Y = date.getFullYear();
-	const M = date.getMonth();
-	const D = date.getDate();
-	const d = date.getDay();
-	const h = date.getHours();
-	const m = date.getMinutes();
-	return template
-		.replace(/%YYYY/g, Y.toString())
-		.replace(/%YY/g, leftpad(Y % century))
-		.replace(/%MMMM/g, MonthNames[M])
-		.replace(/%MMM/g, MonthNames[M].slice(0, shortenedLength))
-		.replace(/%MM/g, leftpad(M + 1))
-		.replace(/%M/g, (M + 1).toString())
-		.replace(/%DD/g, leftpad(D))
-		.replace(/%D/g, D.toString())
-		.replace(/%dddd/g, DayNames[d])
-		.replace(/%ddd/g, DayNames[d].slice(0, shortenedLength))
-		.replace(/%hh/, leftpad(h))
-		.replace(/%h/, h.toString())
-		.replace(/%mm/, leftpad(m))
-		.replace(/%m/, m.toString());
+	if (0 < date) {
+		const Y = date.getFullYear();
+		const M = date.getMonth();
+		const D = date.getDate();
+		const d = date.getDay();
+		const h = date.getHours();
+		const m = date.getMinutes();
+		return template
+			.replace(/%YYYY/g, Y.toString())
+			.replace(/%YY/g, leftpad(Y % century))
+			.replace(/%MMMM/g, MonthNames[M])
+			.replace(/%MMM/g, MonthNames[M].slice(0, shortenedLength))
+			.replace(/%MM/g, leftpad(M + 1))
+			.replace(/%M/g, (M + 1).toString())
+			.replace(/%DD/g, leftpad(D))
+			.replace(/%D/g, D.toString())
+			.replace(/%dddd/g, DayNames[d])
+			.replace(/%ddd/g, DayNames[d].slice(0, shortenedLength))
+			.replace(/%hh/, leftpad(h))
+			.replace(/%h/, h.toString())
+			.replace(/%mm/, leftpad(m))
+			.replace(/%m/, m.toString());
+	}
+	return '';
 }
 
 export default format;
