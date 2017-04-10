034cbc14b78a0a75cce8b22af9aa692b058cf1db
Kei Ito <kei.itof@gmail.com>
2017-04-10 11:03:26 +0900

    fixes dom/children to check nodeType

---
+++ package/Array/from/polyfill.js
@@ -0,0 +1,5 @@
+import Array from '..';
+import arrayFrom from '.';
+if (!Array.from) {
+	Array.from = arrayFrom;
+}
--- package/dom/children/index.mjs
+++ package/dom/children/index.mjs
@@ -1,5 +1,10 @@
+import filter from '../../Array/filter';
+import childNodes from '../childNodes';
+
 function children(node) {
-	return node.children;
+	return filter(childNodes(node), function ({nodeType = 0}) {
+		return nodeType === 1;
+	});
 }
 
 export default children;
