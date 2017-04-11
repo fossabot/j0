7bcfb0647567e13fee7342c21752f68955b5265a
Kei Ito <kei.itof@gmail.com>
2017-04-11 10:32:31 +0900

    removes old modules

--- mod/insertRuleSet.js
+++
@@ -1,9 +0,0 @@
-var forEach = require('../lib/forEach');
-var unnestRuleSet = require('./unnestRuleSet');
-var compileRuleSet = require('./compileRuleSet');
-
-module.exports = function (styleSheet, ruleSets) {
-	forEach(unnestRuleSet(ruleSets), function (flattenedRuleSet) {
-		styleSheet.insertRule(compileRuleSet(flattenedRuleSet), styleSheet.cssRules.length);
-	});
-};
