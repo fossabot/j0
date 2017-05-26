ad90cb906e72d7761c7dbba59505bf8cbda635f4
Kei Ito <kei.itof@gmail.com>
2017-05-26 16:17:31 +0900

    renamed polyfill in HTMLCollection

--- HTMLCollection/@iterator/polyfill.js
+++ HTMLCollection/@iterator/polyfill.js
@@ -2,7 +2,7 @@ import {
 	HTMLCollection,
 	iteratorSymbol
 } from 'j0';
-import generator from './j0';
+import generator from './j0polyfill';
 const {prototype} = HTMLCollection;
 if (!prototype[iteratorSymbol]) {
 	prototype[iteratorSymbol] = generator;
