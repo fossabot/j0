d1c1480aede1979903ec008ae1e81d47e5af73aa
Kei Ito <kei.itof@gmail.com>
2017-05-31 18:45:57 +0900

    added Math.imul

---
+++ Math/imul/j0polyfill/test/index.js
@@ -0,0 +1,3 @@
+import test from '../../tests';
+import imul from '..';
+test(imul, 'Math.imul#j0');
---
+++ Math/imul/tests.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-magic-numbers */
+function test(imul, name = 'Math.imul') {
+
+	describe(name, function () {
+
+		it('imul(2, 4) should be 8', function () {
+			assert.equal(imul(2, 4), 8);
+		});
+
+		it('imul(-1, 8) should be -8', function () {
+			assert.equal(imul(-1, 8), -8);
+		});
+
+		it('imul(-2, -2) should be 4', function () {
+			assert.equal(imul(-2, -2), 4);
+		});
+
+		it('imul(0xffffffff, 5) should be -5', function () {
+			assert.equal(imul(0xffffffff, 5), -5);
+		});
+
+		it('imul(0xfffffffe, 5) should be -10', function () {
+			assert.equal(imul(0xfffffffe, 5), -10);
+		});
+
+	});
+
+}
+export default test;
