d7a54470dde4b83f8b2c3aa87200467299c8770b
Kei Ito <kei.itof@gmail.com>
2017-06-06 19:17:27 +0900

    updated the image

--- thermalRGB/test/index.js
+++ thermalRGB/test/index.js
@@ -14,6 +14,23 @@ describe('thermalRGB', function () {
 			assert.deepEqual(thermalRGB.css(1), 'rgb(255,0,0)');
 		});
 
+		it('[id:thermalRGB] should draw an expected map', async function () {
+			const timeout = 5000;
+			this.timeout(timeout);
+			await assert.graphicalEqual({
+				name: 'thermalRGB',
+				url: `${window.root}/thermalRGB/thermalRGB.png`,
+				fn: function (x, y) {
+					return y;
+				},
+				xRange: [0, 1],
+				yRange: [0, 1],
+				zRange: [0, 1],
+				xGrid: [],
+				yGrid: []
+			});
+		});
+
 	});
 
 });
