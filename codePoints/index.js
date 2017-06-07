5d06842e87489f3d2d44bf9043c1c6cf498fa54d
Kei Ito <kei.itof@gmail.com>
2017-06-07 16:41:27 +0900

    updated codePoints

--- codePoints/index.js
+++ codePoints/index.js
@@ -1,27 +1,184 @@
-/* eslint-disable no-magic-numbers, max-len, no-control-regex */
+/* eslint-disable no-magic-numbers, no-control-regex */
 // https://infra.spec.whatwg.org/#code-points
 import {
-	CharRegExp
+	ConditionalSet
 } from 'j0';
-export const surrogate = CharRegExp.compile(/[\uD800-\uDFFF]/);
-export const scalarValue = CharRegExp.compile(/[^\uD800-\uDFFF]/);
-export const noncharacter = CharRegExp.compile(/[\uFDD0-\uFDEF\uFFFE\uFFFF\u1FFFE\u1FFFF\u2FFFE\u2FFFF\u3FFFE\u3FFFF\u4FFFE\u4FFFF\u5FFFE\u5FFFF\u6FFFE\u6FFFF\u7FFFE\u7FFFF\u8FFFE\u8FFFF\u9FFFE\u9FFFF\uAFFFE\uAFFFF\uBFFFE\uBFFFF\uCFFFE\uCFFFF\uDFFFE\uDFFFF\uEFFFE\uEFFFF\uFFFFE\uFFFFF\u10FFFE\u10FFFF]/);
-export const ASCIICodePoint = CharRegExp.compile(/[\x00-\x7F]/);
-export const ASCIITabOrNewline = CharRegExp.compile(/[\x09\x0A\x0D]/);
-export const ASCIIWhitespace = CharRegExp.compile(/[\x09\x0A\x0C\x0D\x20]/);
-export const C0Control = CharRegExp.compile(/[\x00-\x1F]/);
-export const C0ControlOrSpace = CharRegExp.compile(C0Control, /\x20/);
-export const control = CharRegExp.compile(C0Control, /[\x7F-\x9F]/);
-export const ASCIIDigit = CharRegExp.compile(/[\x30-\x39]/);
-export const ASCIIUpperHexDigit = CharRegExp.compile(ASCIIDigit, /[\x41-\x46]/);
-export const ASCIILowerHexDigit = CharRegExp.compile(ASCIIDigit, /[\x61-\x66]/);
-export const ASCIIHexDigit = CharRegExp.compile(ASCIIUpperHexDigit, ASCIILowerHexDigit);
-export const ASCIIUpperAlpha = CharRegExp.compile(/[\x41-\x5A]/);
-export const ASCIILowerAlpha = CharRegExp.compile(/[\x61-\x7A]/);
-export const ASCIIAlpha = CharRegExp.compile(ASCIIUpperAlpha, ASCIILowerAlpha);
-export const ASCIIAlphanumeric = CharRegExp.compile(ASCIIDigit, ASCIIAlpha);
+export const surrogate = ConditionalSet.solo(function (x) {
+	return 0xD800 <= x && x <= 0xDFFF;
+});
+export const scalarValue = ConditionalSet.not(surrogate);
+export const noncharacter = ConditionalSet.or(
+	function (x) {
+		return 0xFDD0 <= x && x <= 0xFDEF;
+	},
+	function (x) {
+		return [
+			0xFFFE,
+			0xFFFF,
+			0x1FFFE,
+			0x1FFFF,
+			0x2FFFE,
+			0x2FFFF,
+			0x3FFFE,
+			0x3FFFF,
+			0x4FFFE,
+			0x4FFFF,
+			0x5FFFE,
+			0x5FFFF,
+			0x6FFFE,
+			0x6FFFF,
+			0x7FFFE,
+			0x7FFFF,
+			0x8FFFE,
+			0x8FFFF,
+			0x9FFFE,
+			0x9FFFF,
+			0xAFFFE,
+			0xAFFFF,
+			0xBFFFE,
+			0xBFFFF,
+			0xCFFFE,
+			0xCFFFF,
+			0xDFFFE,
+			0xDFFFF,
+			0xEFFFE,
+			0xEFFFF,
+			0xFFFFE,
+			0xFFFFF,
+			0x10FFFE,
+			0x10FFFF
+		]
+		.includes(x);
+	}
+);
+export const ASCIICodePoint = ConditionalSet.solo(function (x) {
+	return 0 <= x && x <= 0x7F;
+});
+export const ASCIITabOrNewline = ConditionalSet.solo(function (x) {
+	return [
+		0x09,
+		0x0A,
+		0x0D
+	]
+	.includes(x);
+});
+export const ASCIIWhitespace = ConditionalSet.solo(function (x) {
+	return [
+		0x09,
+		0x0A,
+		0x0C,
+		0x0D,
+		0x20
+	]
+	.includes(x);
+});
+export const C0Control = ConditionalSet.solo(function (x) {
+	return 0 <= x && x <= 0x1F;
+});
+export const C0ControlOrSpace = ConditionalSet.or(
+	C0Control,
+	function (x) {
+		return x === 0x20;
+	}
+);
+export const control = ConditionalSet.or(
+	C0Control,
+	function (x) {
+		return 0x7F <= x && x <= 0x9F;
+	}
+);
+export const ASCIIDigit = ConditionalSet.solo(function (x) {
+	return 0x30 <= x && x <= 0x39;
+});
+export const ASCIIUpperHexDigit = ConditionalSet.or(
+	ASCIIDigit,
+	function (x) {
+		return 0x41 <= x && x <= 0x46;
+	}
+);
+export const ASCIILowerHexDigit = ConditionalSet.or(
+	ASCIIDigit,
+	function (x) {
+		return 0x61 <= x && x <= 0x66;
+	}
+);
+export const ASCIIHexDigit = ConditionalSet.or(
+	ASCIIUpperHexDigit,
+	ASCIILowerHexDigit
+);
+export const ASCIIUpperAlpha = ConditionalSet.solo(function (x) {
+	return 0x41 <= x && x <= 0x5A;
+});
+export const ASCIILowerAlpha = ConditionalSet.solo(function (x) {
+	return 0x61 <= x && x <= 0x7A;
+});
+export const ASCIIAlpha = ConditionalSet.or(
+	ASCIIUpperAlpha,
+	ASCIILowerAlpha
+);
+export const ASCIIAlphanumeric = ConditionalSet.or(
+	ASCIIDigit,
+	ASCIIAlpha
+);
 
-export const C0ControlPercentEncodeSet = CharRegExp.compile(/[^\x20-\x7D]/);
-export const pathPercentEncodeSet = CharRegExp.compile(C0ControlPercentEncodeSet, /[\x20\x22\x23\x3C\x3E\x3F\x60\x7B\x7D]/);
-export const userinfoPercentEncodeSet = CharRegExp.compile(pathPercentEncodeSet, /[\x2F\x3A\x3B\x3D\x40\x5B\x5C\x5D\x5E\x7C]/);
-export const forbiddenHost = CharRegExp.compile(/[\x00\x09\x0A\x0D\x20\x23\x25\x2F\x3A\x3F\x40\x5B\x5C\x5D]/);
+export const C0ControlPercentEncodeSet = ConditionalSet.or(
+	C0Control,
+	function (x) {
+		return 0x7E < x;
+	}
+);
+
+export const pathPercentEncodeSet = ConditionalSet.or(
+	C0ControlPercentEncodeSet,
+	function (x) {
+		return [
+			0x20,
+			0x22,
+			0x23,
+			0x3C,
+			0x3E,
+			0x3F,
+			0x60,
+			0x7B,
+			0x7D
+		]
+		.includes(x);
+	}
+);
+export const userinfoPercentEncodeSet = ConditionalSet.or(
+	pathPercentEncodeSet,
+	function (x) {
+		return [
+			0x2F,
+			0x3A,
+			0x3B,
+			0x3D,
+			0x40,
+			0x5B,
+			0x5C,
+			0x5D,
+			0x5E,
+			0x7C
+		]
+		.includes(x);
+	}
+);
+export const forbiddenHost = ConditionalSet.solo(function (x) {
+	return [
+		0x00,
+		0x09,
+		0x0A,
+		0x0D,
+		0x20,
+		0x23,
+		0x25,
+		0x2F,
+		0x3A,
+		0x3F,
+		0x40,
+		0x5B,
+		0x5C,
+		0x5D
+	]
+	.includes(x);
+});
