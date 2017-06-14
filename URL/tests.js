6945f549d61bc12667bc5635abec67a7848a6515
Kei Ito <kei.itof@gmail.com>
2017-06-14 14:13:04 +0900

    updated tests for URL

--- URL/tests.js
+++ URL/tests.js
@@ -2,6 +2,47 @@ function test(URL, name = 'URL') {
 
 	describe(name, function () {
 
+		[
+			[
+				['http://example\t.\norg', 'http://example.org/foo/bar'],
+				[
+					['protocol', 'http:'],
+					['host', 'example.org'],
+					['hostname', 'example.org'],
+					['pathname', '/']
+				]
+			],
+			[
+				['http://user:pass@foo:21/bar;par?b#c', 'http://example.org/foo/bar'],
+				[
+					['protocol', 'http:'],
+					['hostname', 'foo'],
+					['port', '21'],
+					['host', 'foo:21'],
+					['pathname', '/bar;par'],
+					['search', '?b'],
+					['hash', '#c']
+				]
+			]
+		]
+		.forEach(([input, tests], index) => {
+			if (tests) {
+				it(`should construct a new URL #${index} ${input}`, function () {
+					const url = new URL(...input);
+					tests
+					.forEach(([key, expected]) => {
+						assert.equal(url[key], expected, `${input}:${key}`);
+					});
+				});
+			} else {
+				it(`should fail to construct a new URL #${index} ${input}`, function () {
+					assert.throws(() => {
+						return new URL(...input);
+					});
+				});
+			}
+		});
+
 		it('should have createObjectURL', function () {
 			assert.equal(typeof URL.createObjectURL, 'function');
 		});
