69916033519fa86fc0db400db44daa54449a9171
Kei Ito <kei.itof@gmail.com>
2017-07-06 13:22:08 +0900

    change sequence

--- StateManager/index.js
+++ StateManager/index.js
@@ -106,9 +106,9 @@ class StateManager extends EventEmitter {
 		if (this.is(newState)) {
 			return;
 		}
-		this.emit('change', newState, this.current);
 		this.current = newState;
 		history[method](newState.name, newState.params, newState.href);
+		this.emit('change', newState, this.current);
 	}
 
 	go(stateInfo) {
