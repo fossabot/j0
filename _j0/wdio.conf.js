2dac52329eca0f0c4524e05cffe31165edcf7224
Kei Ito <kei.itof@gmail.com>
2017-07-06 13:25:43 +0900

    update

--- _j0/wdio.conf.js
+++ _j0/wdio.conf.js
@@ -40,7 +40,7 @@ exports.config = {
 	// and 30 processes will get spawned. The property handles how many capabilities
 	// from the same test should run tests.
 	//
-	maxInstances: 3,
+	// maxInstances: 3,
 	//
 	// If you have trouble getting all important capabilities together, check out the
 	// Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -51,12 +51,11 @@ exports.config = {
 			// maxInstances can get overwritten per capability. So if you have an in-house Selenium
 			// grid with only 5 firefox instances available you can make sure that not more than
 			// 5 instances get started at a time.
-			maxInstances: 3,
+			// maxInstances: 3,
 			browserName: 'chrome',
 			chromeOptions: {
 				args: [
-					'headless',
-					'disable-gpu'
+					'--headless'
 				]
 			}
 		}
