b15967fccaa3b8c6d12f15fe1496e9204c2d3c69
Kei Ito <kei.itof@gmail.com>
2017-07-24 18:32:25 +0900

    fix test

--- dom/test/bb/index.js
+++ dom/test/bb/index.js
@@ -1,9 +1,9 @@
 import dom from '../..';
 
-describe('J0Element.prototype.getBB', function () {
+describe('J0Element.prototype.bb', function () {
 
 	it('should get a bounding box', function () {
-		const {left, top} = dom().getBB();
+		const {left, top} = dom().bb;
 		assert.equal(left, 0);
 		assert.equal(top, 0);
 	});
