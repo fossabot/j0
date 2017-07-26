89a3e2d1a77495d921f29c5c7093137f9fb6a250
Kei Ito <kei.itof@gmail.com>
2017-07-26 15:06:00 +0900

    add polyfill generator

--- build/constants/index.js
+++ build/constants/index.js
@@ -2,9 +2,19 @@ const path = require('path');
 const projectDir = path.join(__dirname, '..', '..');
 const docsDir = path.join(projectDir, 'docs');
 const libDir = path.join(projectDir, 'lib');
+const polyfillDir = path.join(projectDir, 'polyfill');
 module.exports = {
 	projectDir,
 	docsDir,
 	libDir,
-	watch: process.argv.includes('--watch')
+	polyfillDir,
+	watch: process.argv.includes('--watch'),
+	polyfills: [
+		'polyfill-symbol.js',
+		'polyfill-others.js',
+		'polyfill-regenerator-runtime.js'
+	]
+	.map((name) => {
+		return path.join(docsDir, name);
+	})
 };
--- build/index.js
+++ build/index.js
@@ -1,10 +1,12 @@
 const console = require('j1/console').create('build');
 const copyStaticFilesForTest = require('./copyStaticFilesForTest');
 const rollupScripts = require('./rollupScripts');
+const generatePolyfill = require('./generatePolyfill');
 
 async function build() {
 	await rollupScripts();
-	// await copyStaticFilesForTest();
+	await copyStaticFilesForTest();
+	await generatePolyfill();
 }
 
 module.exports = build;
