34474ca5951ec7605efd97b90e6857d605c2737e
Kei Ito <kei.itof@gmail.com>
2017-07-27 01:16:16 +0900

    access localhost

--- test/index.js
+++ test/index.js
@@ -1,7 +1,10 @@
-const assert = require('assert');
+const path = require('path');
+const SableServer = require('sable');
+const browserstack = require('browserstack-local');
 const {Builder} = require('selenium-webdriver');
 const $console = require('j1/console').create('test');
 const isString = require('j1/isString');
+const promisify = require('j1/promisify');
 const useBrowserStack = require('./useBrowserStack');
 
 function getCapabilityText(capability) {
@@ -29,15 +32,55 @@ async function test(capabilities = require('./capabilities')) {
 	console.info('build a driver');
 	const driver = builder.build();
 	console.info('access');
-	await driver.get('http://webdriver.io/');
+	await driver.get(`http://localhost:${exports.port}/`);
 	const title = await driver.getTitle();
 	console.info(title);
-	assert.equal((/webdriver/i).test(title), true);
 	await driver.quit();
 	await test(capabilities);
 }
 
-test()
+async function onEnd() {
+	if (exports.server) {
+		await exports.server.close();
+	}
+	if (exports.bsLocal) {
+		await exports.bsLocal.stop();
+	}
+	if (exports.driver) {
+		await exports.driver.quit();
+	}
+}
+
+Promise.resolve()
+.then(async () => {
+	const server = new SableServer({
+		documentRoot: path.join(__dirname, '..', 'docs'),
+		noWatch: true,
+		quiet: true
+	});
+	exports.server = server;
+	await server.start();
+	const {port} = server.address();
+	exports.port = port;
+	if (useBrowserStack) {
+		const bsLocal = new browserstack.Local();
+		exports.bsLocal = bsLocal;
+		await promisify(bsLocal.start, bsLocal)({
+			// https://github.com/browserstack/browserstack-local-nodejs/blob/master/lib/Local.js
+			verbose: true,
+			forceLocal: true,
+			onlyAutomate: true,
+			onlyCommand: true,
+			only: `localhost,${port},0`
+		});
+	}
+	await test();
+	await onEnd();
+})
+.catch(async (error) => {
+	await onEnd();
+	throw error;
+})
 .catch((error) => {
 	$console.onError(error);
 	process.exit(1);
