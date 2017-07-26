2029f57c84d31b487ec8ed0c705bd307c69fcbb3
Kei Ito <kei.itof@gmail.com>
2017-07-26 16:24:50 +0900

    watch files and update scripts automatically

--- build/index.js
+++ build/index.js
@@ -1,7 +1,15 @@
+const path = require('path');
+const chokidar = require('chokidar');
+const SableServer = require('sable');
 const console = require('j1/console').create('build');
 const copyStaticFilesForTest = require('./copyStaticFilesForTest');
 const rollupScripts = require('./rollupScripts');
 const copyDependencies = require('./copyDependencies');
+const {
+	watch,
+	libDir,
+	docsDir
+} = require('./constants');
 
 async function build() {
 	await copyDependencies();
@@ -13,6 +21,28 @@ module.exports = build;
 
 if (!module.parent) {
 	build()
+	.then(async () => {
+		if (watch) {
+			await new SableServer({
+				documentRoot: docsDir
+			})
+			.start();
+			chokidar.watch(libDir, {
+				awaitWriteFinish: {
+					stabilityThreshold: 100
+				}
+			})
+			.on('change', function (file) {
+				switch (path.extname(file)) {
+				case '.js':
+					rollupScripts(file).catch(console.onError);
+					break;
+				default:
+					copyStaticFilesForTest().catch(console.onError);
+				}
+			});
+		}
+	})
 	.catch((error) => {
 		console.onError(error);
 		process.exit(1);
--- build/rollupScripts/index.js
+++ build/rollupScripts/index.js
@@ -12,33 +12,54 @@ const {
 	projectDir,
 	docsDir
 } = require('../constants');
+const dependenciesList = new Map();
 
-async function rollupScripts() {
-	const pattern = path.join(docsDir, '**', '*.rollup.js');
-	console.info('search files to rollup', pattern);
-	const files = await glob(pattern, {nodir: true});
-	console.info(`found ${files.length} files`);
+async function rollupScript(entry) {
+	const $console = console.create(`rollupScripts:${path.relative(docsDir, entry)}`);
+	$console.info('rollup');
+	const bundle = await rollup({
+		entry,
+		plugins: [
+			j0(),
+			globImport(),
+			commonjs()
+		]
+	});
+	$console.info('generate');
+	const {code} = await bundle.generate({format: 'es'});
+	$console.info(`dependencies: ${bundle.modules.length}`);
+	dependenciesList.set(entry, new Set(
+		bundle.modules.map(({id}) => {
+			return id;
+		})
+	));
+	$console.info('transpile');
+	const {code: babeledCode} = babel.transform(code, {presets: ['env']});
+	const wrappedCode = `(function(){\n${babeledCode}\n}())`;
+	const dest = entry.replace(/\.rollup\.js/, '.js');
+	$console.info(`write to ${path.relative(projectDir, dest)}`);
+	await writeFile(dest, wrappedCode);
+	$console.info('done');
+}
+
+async function rollupScripts(updatedFile) {
+	const files = [];
+	if (updatedFile) {
+		console.info(`search files dependent on ${updatedFile}`);
+		for (const [entry, dependencies] of dependenciesList) {
+			if (dependencies.has(updatedFile)) {
+				console.info(entry);
+				files.push(entry);
+			}
+		}
+	} else {
+		const pattern = path.join(docsDir, '**', '*.rollup.js');
+		console.info(`search files to rollup (${pattern})`);
+		files.push(...(await glob(pattern, {nodir: true})));
+	}
+	console.info(`rollup ${files.length} files`);
 	for (const entry of files) {
-		const $console = console.create(`rollupScripts:${path.relative(docsDir, entry)}`);
-		$console.info('rollup');
-		const bundle = await rollup({
-			entry,
-			plugins: [
-				j0(),
-				globImport(),
-				commonjs()
-			]
-		});
-		$console.info('generate');
-		const {code} = await bundle.generate({format: 'es'});
-		$console.info(`dependencies: ${bundle.modules.length}`);
-		$console.info('transpile');
-		const {code: babeledCode} = babel.transform(code, {presets: ['env']});
-		const wrappedCode = `(function(){\n${babeledCode}\n}())`;
-		const dest = entry.replace(/\.rollup\.js/, '.js');
-		$console.info(`write to ${path.relative(projectDir, dest)}`);
-		await writeFile(dest, wrappedCode);
-		$console.info('done');
+		await rollupScript(entry);
 	}
 	console.info('done');
 }
