cdd5d80b5ae69d7988606884134607ae4032eaa7
Kei Ito <kei.itof@gmail.com>
2017-07-28 00:10:34 +0900

    add capabilities

--- test/capabilities/index.js
+++ test/capabilities/index.js
@@ -12,6 +12,11 @@ if (useBrowserStack) {
 			os_version: 'Sierra',
 			browserName: 'Firefox'
 		},
+		{
+			os: 'OS X',
+			os_version: 'Sierra',
+			browserName: 'Opera'
+		},
 		// {
 		// 	os: 'OS X',
 		// 	os_version: 'Sierra',
@@ -20,15 +25,48 @@ if (useBrowserStack) {
 		{
 			os: 'Windows',
 			os_version: '10',
-			browserName: 'IE',
-			browser_version: '11'
+			browserName: 'IE'
+		},
+		{
+			os: 'Windows',
+			os_version: '10',
+			browserName: 'Chrome'
+		},
+		{
+			os: 'Windows',
+			os_version: '10',
+			browserName: 'Firefox'
+		},
+		{
+			os: 'Windows',
+			os_version: '10',
+			browserName: 'Opera'
+		},
+		{
+			device: 'iPhone 7',
+			realMobile: 'true',
+			os_version: '10.0'
+		},
+		{
+			browserName: 'iPad',
+			platform: 'MAC',
+			device: 'iPad Pro'
+		},
+		{
+			device: 'Google Pixel',
+			realMobile: 'true',
+			os_version: '7.1'
+		},
+		{
+			device: 'Google Nexus 9',
+			realMobile: 'true',
+			os_version: '5.1'
+		},
+		{
+			device: 'Samsung Galaxy S7',
+			realMobile: 'true',
+			os_version: '6.0'
 		}
-		// {
-		// 	os: 'Windows',
-		// 	os_version: '10',
-		// 	browserName: 'IE',
-		// 	browser_version: '10'
-		// }
 	];
 } else {
 	module.exports = [
