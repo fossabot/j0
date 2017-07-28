76b466a43b38b0d1f4909af6c0f52ff3c15e7858
Kei Ito <kei.itof@gmail.com>
2017-07-28 14:46:22 +0900

    add device

--- test/capabilities/index.js
+++ test/capabilities/index.js
@@ -1,45 +1,40 @@
 const useBrowserStack = require('../useBrowserStack');
 if (useBrowserStack) {
 	module.exports = [
-		// {
-		// 	os: 'OS X',
-		// 	os_version: 'Sierra',
-		// 	browserName: 'Chrome'
-		// },
-		// {
-		// 	os: 'OS X',
-		// 	os_version: 'Sierra',
-		// 	browserName: 'Firefox'
-		// },
+		{
+			os: 'OS X',
+			os_version: 'Sierra',
+			browserName: 'Chrome'
+		},
+		{
+			os: 'OS X',
+			os_version: 'Sierra',
+			browserName: 'Firefox'
+		},
 		{
 			os: 'OS X',
 			os_version: 'Sierra',
 			browserName: 'Safari'
 		},
-		// {
-		// 	os: 'Windows',
-		// 	os_version: '10',
-		// 	browserName: 'IE'
-		// },
-		// {
-		// 	os: 'Windows',
-		// 	os_version: '10',
-		// 	browserName: 'Chrome'
-		// },
-		// {
-		// 	os: 'Windows',
-		// 	os_version: '10',
-		// 	browserName: 'Firefox'
-		// },
-		// {
-		// 	browserName: 'iPhone',
-		// 	platform: 'MAC',
-		// 	device: 'iPhone 6S'
-		// },
 		{
-			browserName: 'android',
-			platform: 'ANDROID',
-			device: 'Google Nexus 5'
+			os: 'Windows',
+			os_version: '10',
+			browserName: 'IE'
+		},
+		{
+			os: 'Windows',
+			os_version: '10',
+			browserName: 'Chrome'
+		},
+		{
+			os: 'Windows',
+			os_version: '10',
+			browserName: 'Firefox'
+		},
+		{
+			browserName: 'iPhone',
+			platform: 'MAC',
+			device: 'iPhone 6S'
 		}
 	];
 } else {
--- test/getCapabilityText/index.js
+++ test/getCapabilityText/index.js
@@ -2,7 +2,7 @@ const isString = require('j1/isString');
 
 function getCapabilityText(capability = {}) {
 	const fields = [];
-	for (const key of ['os', 'os_version', 'browserName', 'browser_version', 'resolution']) {
+	for (const key of ['device', 'os', 'os_version', 'browserName', 'browser_version', 'resolution']) {
 		const value = capability[key];
 		if (isString(value)) {
 			fields.push(value);
