dc1e6ebf1f0c8c6196ef5d4996031f0fbed3580d
Kei Ito <kei.itof@gmail.com>
2017-07-28 14:37:59 +0900

    test Nexus 9

--- test/capabilities/index.js
+++ test/capabilities/index.js
@@ -1,50 +1,45 @@
 const useBrowserStack = require('../useBrowserStack');
 if (useBrowserStack) {
 	module.exports = [
-		{
-			os: 'OS X',
-			os_version: 'Sierra',
-			browserName: 'Chrome'
-		},
-		{
-			os: 'OS X',
-			os_version: 'Sierra',
-			browserName: 'Firefox'
-		},
 		// {
 		// 	os: 'OS X',
 		// 	os_version: 'Sierra',
-		// 	browserName: 'Opera'
+		// 	browserName: 'Chrome'
+		// },
+		// {
+		// 	os: 'OS X',
+		// 	os_version: 'Sierra',
+		// 	browserName: 'Firefox'
 		// },
 		{
 			os: 'OS X',
 			os_version: 'Sierra',
 			browserName: 'Safari'
 		},
-		{
-			os: 'Windows',
-			os_version: '10',
-			browserName: 'IE'
-		},
-		{
-			os: 'Windows',
-			os_version: '10',
-			browserName: 'Chrome'
-		},
-		{
-			os: 'Windows',
-			os_version: '10',
-			browserName: 'Firefox'
-		},
-		{
-			browserName: 'iPhone',
-			platform: 'MAC',
-			device: 'iPhone 6S'
-		},
+		// {
+		// 	os: 'Windows',
+		// 	os_version: '10',
+		// 	browserName: 'IE'
+		// },
+		// {
+		// 	os: 'Windows',
+		// 	os_version: '10',
+		// 	browserName: 'Chrome'
+		// },
+		// {
+		// 	os: 'Windows',
+		// 	os_version: '10',
+		// 	browserName: 'Firefox'
+		// },
+		// {
+		// 	browserName: 'iPhone',
+		// 	platform: 'MAC',
+		// 	device: 'iPhone 6S'
+		// },
 		{
 			browserName: 'android',
 			platform: 'ANDROID',
-			device: 'Google Nexus 7'
+			device: 'Google Nexus 9'
 		}
 	];
 } else {
