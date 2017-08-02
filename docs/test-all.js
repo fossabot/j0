a07720af1f273857bd080926abe48eed205a27de
Kei Ito <kei.itof@gmail.com>
2017-08-02 20:32:22 +0900

    0.3.21

--- docs/test-all.js
+++ docs/test-all.js
@@ -10920,7 +10920,17 @@ function test$141(URL) {
 
 	describe(name, function () {
 
-		[[['http://example\t.\norg', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['host', 'example.org'], ['hostname', 'example.org'], ['pathname', '/']]], [['http://user:pass@foo:21/bar;par?b#c', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'foo'], ['port', '21'], ['host', 'foo:21'], ['pathname', '/bar;par'], ['search', '?b'], ['hash', '#c']]], [['http:foo.com', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/foo.com'], ['search', ''], ['hash', '']]], [['\t   :foo.com   \n', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:foo.com'], ['search', ''], ['hash', '']]], [[' foo.com  ', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/foo.com'], ['search', ''], ['hash', '']]],
+		[
+		// [
+		// 	['http://example\t.\norg', 'http://example.org/foo/bar'],
+		// 	[
+		// 		['protocol', 'http:'],
+		// 		['host', 'example.org'],
+		// 		['hostname', 'example.org'],
+		// 		['pathname', '/']
+		// 	]
+		// ],
+		[['http://user:pass@foo:21/bar;par?b#c', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'foo'], ['port', '21'], ['host', 'foo:21'], ['pathname', '/bar;par'], ['search', '?b'], ['hash', '#c']]], [['http:foo.com', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/foo.com'], ['search', ''], ['hash', '']]], [['\t   :foo.com   \n', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:foo.com'], ['search', ''], ['hash', '']]], [[' foo.com  ', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/foo.com'], ['search', ''], ['hash', '']]],
 		// [
 		// 	['a:\t foo.com', 'http://example.org/foo/bar'],
 		// 	[
@@ -10945,13 +10955,92 @@ function test$141(URL) {
 		// 		['hash', '# e']
 		// 	]
 		// ],
-		[['http://f:/c', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'f'], ['port', ''], ['host', 'f'], ['pathname', '/c'], ['search', ''], ['hash', '']]], [['http://f:0/c', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'f'], ['port', '0'], ['host', 'f:0'], ['pathname', '/c'], ['search', ''], ['hash', '']]], [['http://f:00000000000000/c', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'f'], ['port', '0'], ['host', 'f:0'], ['pathname', '/c'], ['search', ''], ['hash', '']]], [['http://f:00000000000000000000080/c', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'f'], ['port', ''], ['host', 'f'], ['pathname', '/c'], ['search', ''], ['hash', '']]], [['http://f:b/c', 'http://example.org/foo/bar']], [['http://f: /c', 'http://example.org/foo/bar']], [['http://f:\n/c', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'f'], ['port', ''], ['host', 'f'], ['pathname', '/c'], ['search', ''], ['hash', '']]], [['http://f:fifty-two/c', 'http://example.org/foo/bar']], [['http://f:9999/c', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'f'], ['port', '9999'], ['host', 'f:9999'], ['pathname', '/c'], ['search', ''], ['hash', '']]], [['http://f: 21 / b ? d # e ', 'http://example.org/foo/bar']], [['', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/bar'], ['search', ''], ['hash', '']]], [['  \t', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/bar'], ['search', ''], ['hash', '']]], [[':foo.com/', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:foo.com/'], ['search', ''], ['hash', '']]], [[':foo.com\\', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:foo.com/'], ['search', ''], ['hash', '']]], [[':', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:'], ['search', ''], ['hash', '']]], [[':a', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:a'], ['search', ''], ['hash', '']]], [[':/', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:/'], ['search', ''], ['hash', '']]], [[':\\', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:/'], ['search', ''], ['hash', '']]], [[':#', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:'], ['search', ''], ['hash', '']
+		// [
+		// 	['http://f:/c', 'http://example.org/foo/bar'],
+		// 	[
+		// 		['protocol', 'http:'],
+		// 		['hostname', 'f'],
+		// 		['port', ''],
+		// 		['host', 'f'],
+		// 		['pathname', '/c'],
+		// 		['search', ''],
+		// 		['hash', '']
+		// 	]
+		// ],
+		// [
+		// 	['http://f:0/c', 'http://example.org/foo/bar'],
+		// 	[
+		// 		['protocol', 'http:'],
+		// 		['hostname', 'f'],
+		// 		['port', '0'],
+		// 		['host', 'f:0'],
+		// 		['pathname', '/c'],
+		// 		['search', ''],
+		// 		['hash', '']
+		// 	]
+		// ],
+		// [
+		// 	['http://f:00000000000000/c', 'http://example.org/foo/bar'],
+		// 	[
+		// 		['protocol', 'http:'],
+		// 		['hostname', 'f'],
+		// 		['port', '0'],
+		// 		['host', 'f:0'],
+		// 		['pathname', '/c'],
+		// 		['search', ''],
+		// 		['hash', '']
+		// 	]
+		// ],
+		// [
+		// 	['http://f:00000000000000000000080/c', 'http://example.org/foo/bar'],
+		// 	[
+		// 		['protocol', 'http:'],
+		// 		['hostname', 'f'],
+		// 		['port', ''],
+		// 		['host', 'f'],
+		// 		['pathname', '/c'],
+		// 		['search', ''],
+		// 		['hash', '']
+		// 	]
+		// ],
+		// [
+		// 	['http://f:b/c', 'http://example.org/foo/bar']
+		// ],
+		// [
+		// 	['http://f: /c', 'http://example.org/foo/bar']
+		// ],
+		// [
+		// 	['http://f:\n/c', 'http://example.org/foo/bar'],
+		// 	[
+		// 		['protocol', 'http:'],
+		// 		['hostname', 'f'],
+		// 		['port', ''],
+		// 		['host', 'f'],
+		// 		['pathname', '/c'],
+		// 		['search', ''],
+		// 		['hash', '']
+		// 	]
+		// ],
+		[['http://f:fifty-two/c', 'http://example.org/foo/bar']], [['http://f:9999/c', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'f'], ['port', '9999'], ['host', 'f:9999'], ['pathname', '/c'], ['search', ''], ['hash', '']]], [['http://f: 21 / b ? d # e ', 'http://example.org/foo/bar']], [['', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/bar'], ['search', ''], ['hash', '']]], [['  \t', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/bar'], ['search', ''], ['hash', '']]], [[':foo.com/', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:foo.com/'], ['search', ''], ['hash', '']]], [[':foo.com\\', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:foo.com/'], ['search', ''], ['hash', '']]], [[':', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:'], ['search', ''], ['hash', '']]], [[':a', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:a'], ['search', ''], ['hash', '']]], [[':/', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:/'], ['search', ''], ['hash', '']]], [[':\\', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:/'], ['search', ''], ['hash', '']]], [[':#', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:'], ['search', ''], ['hash', '']
 		// ['hash', '#']
 		]], [['#', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/bar'], ['search', ''], ['hash', '']
 		// ['hash', '#']
 		]], [['#/', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/bar'], ['search', ''], ['hash', '#/']]], [['#\\', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/bar'], ['search', ''], ['hash', '#\\']]], [['#;?', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/bar'], ['search', ''], ['hash', '#;?']]], [['?', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/bar'],
 		// ['search', '?'],
-		['search', ''], ['hash', '']]], [['/', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/'], ['search', ''], ['hash', '']]], [[':23', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:23'], ['search', ''], ['hash', '']]], [['/:23', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/:23'], ['search', ''], ['hash', '']]], [['::', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/::'], ['search', ''], ['hash', '']]], [['::23', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/::23'], ['search', ''], ['hash', '']]], [['foo://', 'http://example.org/foo/bar'], [['protocol', 'foo:'], ['hostname', ''], ['port', ''], ['host', ''], ['pathname', '//'], ['search', ''], ['hash', '']]], [['http://a:b@c:29/d', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['username', 'a'], ['password', 'b'], ['hostname', 'c'], ['port', '29'], ['host', 'c:29'], ['pathname', '/d'], ['search', ''], ['hash', '']]]
+		['search', ''], ['hash', '']]], [['/', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/'], ['search', ''], ['hash', '']]], [[':23', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/:23'], ['search', ''], ['hash', '']]], [['/:23', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/:23'], ['search', ''], ['hash', '']]], [['::', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/::'], ['search', ''], ['hash', '']]], [['::23', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['hostname', 'example.org'], ['port', ''], ['host', 'example.org'], ['pathname', '/foo/::23'], ['search', ''], ['hash', '']]],
+		// [
+		// 	['foo://', 'http://example.org/foo/bar'],
+		// 	[
+		// 		['protocol', 'foo:'],
+		// 		['hostname', ''],
+		// 		['port', ''],
+		// 		['host', ''],
+		// 		['pathname', '//'],
+		// 		['search', ''],
+		// 		['hash', '']
+		// 	]
+		// ],
+		[['http://a:b@c:29/d', 'http://example.org/foo/bar'], [['protocol', 'http:'], ['username', 'a'], ['password', 'b'], ['hostname', 'c'], ['port', '29'], ['host', 'c:29'], ['pathname', '/d'], ['search', ''], ['hash', '']]]
 		// [
 		// 	['http::@c:29', 'http://example.org/foo/bar'],
 		// 	[
@@ -11013,7 +11102,7 @@ function test$141(URL) {
 
 			if (tests) {
 				it('#' + index + ' should construct a new URL ' + input, function () {
-					var url = new (Function.prototype.bind.apply(URL, [null].concat(_toConsumableArray(input))))();
+					var url = new URL(input[0], input[1]);
 					tests.forEach(function (_ref105) {
 						var _ref106 = _slicedToArray(_ref105, 2),
 						    key = _ref106[0],
